613a1af278a1116dc6a96f14c785221d93d4c173
Jamie Rushford <jar.rushford@gmail.com>
2018-12-22 17:37:28 -0700

    Break out calendar logic into utils helper

--- src/containers/Training/Training.js
+++ src/containers/Training/Training.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect }  from 'react-redux';
 import { withRouter, Link } from 'react-router-dom'
 import { toggleComplete } from '../../actions'
+import * as Helper from '../../utils/calendarHelpers'
 import '../../main.scss'
 
 function Training({ trainingData, toggleComplete }) {
@@ -20,7 +21,6 @@ function Training({ trainingData, toggleComplete }) {
     const keyM = key.substring(0, 2)
     const keyY = key.substring(6)
     if (key === today) {
-
       weekIndex[todayIndex] = (
         <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
           <h3>{trainingData[key].type}</h3>
@@ -28,9 +28,9 @@ function Training({ trainingData, toggleComplete }) {
         </li>
       )
     } else if (endOfMonthIndex.includes(today.substring(3, 5))) {
-      Object.assign(weekIndex, endOfMonthHelper(key, today, weekIndex, todayIndex, trainingData, endOfMonthIndex, toggleComplete))
+      Object.assign(weekIndex, Helper.endOfMonthHelper(key, today, weekIndex, todayIndex, trainingData, endOfMonthIndex, toggleComplete))
     } else if (keyM === todayM && keyY === todayY) {
-      Object.assign(weekIndex, daysOfWeekHelper(key, today, weekIndex, todayIndex, trainingData, toggleComplete))
+      Object.assign(weekIndex, Helper.daysOfWeekHelper(key, today, weekIndex, todayIndex, trainingData, toggleComplete))
     }
   })
 
@@ -46,33 +46,33 @@ function Training({ trainingData, toggleComplete }) {
         </div>
       </div>
       <div className="training">
-        <div className={`day ${highlightHelper('sunday') && 'highlighted'}`}>
-          <h2>Sunday {dateHelper(0)}</h2>
+        <div className={`day ${Helper.highlightHelper('sunday') && 'highlighted'}`}>
+          <h2>Sunday {Helper.dateHelper(0)}</h2>
           <ul>{weekIndex[0]}</ul>
         </div>
-        <div className={`day ${highlightHelper('monday') && 'highlighted'}`}>
-          <h2>Monday {dateHelper(1)}</h2>
+        <div className={`day ${Helper.highlightHelper('monday') && 'highlighted'}`}>
+          <h2>Monday {Helper.dateHelper(1)}</h2>
           <ul>{weekIndex[1]}</ul>
         </div>
         <div
-          className={`day ${highlightHelper('tuesday') && 'highlighted'}`}>
-          <h2>Tuesday {dateHelper(2)}</h2>
+          className={`day ${Helper.highlightHelper('tuesday') && 'highlighted'}`}>
+          <h2>Tuesday {Helper.dateHelper(2)}</h2>
           <ul>{weekIndex[2]}</ul>
         </div>
-        <div className={`day ${highlightHelper('wednesday') && 'highlighted'}`}>
-          <h2>Wednesday {dateHelper(3)}</h2>
+        <div className={`day ${Helper.highlightHelper('wednesday') && 'highlighted'}`}>
+          <h2>Wednesday {Helper.dateHelper(3)}</h2>
           <ul>{weekIndex[3]}</ul>
         </div>
-        <div className={`day ${highlightHelper('thursday') && 'highlighted'}`}>
-          <h2>Thursday {dateHelper(4)}</h2>
+        <div className={`day ${Helper.highlightHelper('thursday') && 'highlighted'}`}>
+          <h2>Thursday {Helper.dateHelper(4)}</h2>
           <ul>{weekIndex[4]}</ul>
         </div>
-        <div className={`day ${highlightHelper('friday') && 'highlighted'}`}>
-          <h2>Friday {dateHelper(5)}</h2>
+        <div className={`day ${Helper.highlightHelper('friday') && 'highlighted'}`}>
+          <h2>Friday {Helper.dateHelper(5)}</h2>
           <ul>{weekIndex[5]}</ul>
         </div>
-        <div className={`day ${highlightHelper('saturday') && 'highlighted'}`}>
-          <h2>Saturday {dateHelper(6)}</h2>
+        <div className={`day ${Helper.highlightHelper('saturday') && 'highlighted'}`}>
+          <h2>Saturday {Helper.dateHelper(6)}</h2>
           <ul>{weekIndex[6]}</ul>
         </div>
       </div>
@@ -89,103 +89,3 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Training))
-
-/*******************
- PRIVATE
- *******************/
-
-const daysOfWeekHelper = (key, today, weekIndex, todayIndex, trainingData, toggleComplete) => {
-  const keyDay = key.substring(3, 5)
-  const todayDay = today.substring(3, 5)
-  if (keyDay > todayDay && keyDay - todayDay <= 6 - todayIndex) {
-    weekIndex[todayIndex + (keyDay - todayDay)] = (
-      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
-        <h3>{trainingData[key].type}</h3>
-        <p>{trainingData[key].description}</p>
-      </li>
-    )
-  } else if (keyDay < todayDay && todayDay - keyDay <= todayIndex) {
-    weekIndex[todayIndex - (todayDay - keyDay)] = (
-      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
-        <h3>{trainingData[key].type}</h3>
-        <p>{trainingData[key].description}</p>
-      </li>
-    )
-  }
-  return weekIndex
-}
-
-const endOfMonthHelper = (key, today, weekIndex, todayIndex, trainingData, toggleComplete) => {
-  const keyDay = key.substring(3, 5)
-  const todayDay = today.substring(3, 5)
-  const distanceUp = daysInThisMonth() - parseInt(todayDay) + parseInt(keyDay)
-  const distanceDown = daysInPreviousMonth() - parseInt(keyDay) + parseInt(todayDay)
-  if (keyDay < todayDay && distanceUp <= 6 - todayIndex) {
-    weekIndex[parseInt(todayIndex) + parseInt(distanceUp)] = (
-      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
-        <h3>{trainingData[key].type}</h3>
-        <p>{trainingData[key].description}</p>
-      </li>
-    )
-  } else if (keyDay > todayDay && distanceDown <= todayIndex) {
-    weekIndex[parseInt(todayIndex) - parseInt(distanceDown)] = (
-      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
-        <h3>{trainingData[key].type}</h3>
-        <p>{trainingData[key].description}</p>
-      </li>
-    )
-  }
-}
-
-const daysInThisMonth = () => {
-  const now = new Date()
-  return new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()
-}
-
-const daysInPreviousMonth = () => {
-  const now = new Date()
-  return new Date(now.getFullYear(), now.getMonth(), 0).getDate()
-}
-
-const highlightHelper = (day) => {
-  const date = new Date()
-  const todayIndex = date.getDay()
-  const daysOfWeek = {
-    sunday: 0,
-    monday: 1,
-    tuesday: 2,
-    wednesday: 3,
-    thursday: 4,
-    friday: 5,
-    saturday: 6
-  }
-  if (daysOfWeek[day] === todayIndex) {
-    return true
-  }
-  return false
-}
-
-const dateHelper = (index) => {
-  const endOfMonthIndex = ['25', '26', '27', '28', '29', '30', '31', '1', '2', '3', '4', '5', '6']
-  const date = new Date()
-  const day = date.getDate()
-  const todayIndex = date.getDay()
-  let adjustment
-
-  if (!endOfMonthIndex.includes(day.toString())) {
-    if (index < todayIndex) {
-      adjustment = day - (todayIndex - index)
-    } else {
-      adjustment = day + (index - todayIndex)
-    }
-  } else {
-    if (index < todayIndex && todayIndex - index >= day) {
-      adjustment = daysInPreviousMonth() - ((todayIndex - day) - index)
-    } else if (index > todayIndex && (index - todayIndex) >= (daysInThisMonth() - day)) {
-      adjustment = (index - todayIndex) - (daysInThisMonth()  - day)
-    } else {
-      adjustment = day - (todayIndex - index)
-    }
-  }
-  return adjustment
-}
---
+++ src/utils/calendarHelpers.js
@@ -0,0 +1,97 @@
+import React from 'react'
+
+export const daysOfWeekHelper = (key, today, weekIndex, todayIndex, trainingData, toggleComplete) => {
+  const keyDay = key.substring(3, 5)
+  const todayDay = today.substring(3, 5)
+  if (keyDay > todayDay && keyDay - todayDay <= 6 - todayIndex) {
+    weekIndex[todayIndex + (keyDay - todayDay)] = (
+      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
+        <h3>{trainingData[key].type}</h3>
+        <p>{trainingData[key].description}</p>
+      </li>
+    )
+  } else if (keyDay < todayDay && todayDay - keyDay <= todayIndex) {
+    weekIndex[todayIndex - (todayDay - keyDay)] = (
+      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
+        <h3>{trainingData[key].type}</h3>
+        <p>{trainingData[key].description}</p>
+      </li>
+    )
+  }
+  return weekIndex
+}
+
+export const endOfMonthHelper = (key, today, weekIndex, todayIndex, trainingData, toggleComplete) => {
+  const keyDay = key.substring(3, 5)
+  const todayDay = today.substring(3, 5)
+  const distanceUp = daysInThisMonth() - parseInt(todayDay) + parseInt(keyDay)
+  const distanceDown = daysInPreviousMonth() - parseInt(keyDay) + parseInt(todayDay)
+  if (keyDay < todayDay && distanceUp <= 6 - todayIndex) {
+    weekIndex[parseInt(todayIndex) + parseInt(distanceUp)] = (
+      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
+        <h3>{trainingData[key].type}</h3>
+        <p>{trainingData[key].description}</p>
+      </li>
+    )
+  } else if (keyDay > todayDay && distanceDown <= todayIndex) {
+    weekIndex[parseInt(todayIndex) - parseInt(distanceDown)] = (
+      <li className={`workout-item ${trainingData[key].completed && 'completed'}`} onClick={() => toggleComplete(key)}>
+        <h3>{trainingData[key].type}</h3>
+        <p>{trainingData[key].description}</p>
+      </li>
+    )
+  }
+}
+
+export const daysInThisMonth = () => {
+  const now = new Date()
+  return new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()
+}
+
+export const daysInPreviousMonth = () => {
+  const now = new Date()
+  return new Date(now.getFullYear(), now.getMonth(), 0).getDate()
+}
+
+export const highlightHelper = (day) => {
+  const date = new Date()
+  const todayIndex = date.getDay()
+  const daysOfWeek = {
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6
+  }
+  if (daysOfWeek[day] === todayIndex) {
+    return true
+  }
+  return false
+}
+
+export const dateHelper = (index) => {
+  const endOfMonthIndex = ['25', '26', '27', '28', '29', '30', '31', '1', '2', '3', '4', '5', '6']
+  const date = new Date()
+  const day = date.getDate()
+  const todayIndex = date.getDay()
+  let adjustment
+
+  if (!endOfMonthIndex.includes(day.toString())) {
+    if (index < todayIndex) {
+      adjustment = day - (todayIndex - index)
+    } else {
+      adjustment = day + (index - todayIndex)
+    }
+  } else {
+    if (index < todayIndex && todayIndex - index >= day) {
+      adjustment = daysInPreviousMonth() - ((todayIndex - day) - index)
+    } else if (index > todayIndex && (index - todayIndex) >= (daysInThisMonth() - day)) {
+      adjustment = (index - todayIndex) - (daysInThisMonth()  - day)
+    } else {
+      adjustment = day - (todayIndex - index)
+    }
+  }
+  return adjustment
+}
