fc796c220b774349a95e23631dde2f5d74dcbfb8
Jamie Rushford <jar.rushford@gmail.com>
2018-12-26 11:55:14 -0500

    Add delete icon

--- src/containers/Milestones/Milestone.js
+++ src/containers/Milestones/Milestone.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Milestone({ milestone_date, caption }) {
   return (
     <article className="milestone">
-      <h3>{milestone_date}</h3>
+      <div>
+        <h3>{milestone_date}</h3>
+        <i className="fas fa-times"></i>
+      </div>
       <p>{caption}</p>
     </article>
   )
